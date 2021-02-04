ea74f3b4dc95fc0a8341687693e2853b7b1b0a3c
JT.Han <59948448+Jiatong001@users.noreply.github.com>
2021-02-04 15:30:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,15 +30,18 @@ st.sidebar.title("Airbnb User Recommendation Service 🏡")
 
 if st.button("I'm a new user", key='new'):
      dest = st.selectbox('Where do you want to go?',['Raffles Place, Marina, Cecil','Tanjong Pagar, Chinatown','Tiong Bahru, Alexandra, Queenstown'],key='dest')
-     df = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],columns=['lat', 'lon'])
-     st.map(df)
-     date_from = st.datatime('From when are you booking?',min_value=datetime.date.today(),key='when1')
-     date_to = st.datatime('To when are you booking?',min_value=date_from,key='when2')
+     date_from = st.date_input('From when are you booking?',min_value=datetime.date.today(),key='when1')
+     date_to = st.date_input('To when are you booking?',min_value=date_from,key='when2')
      price = st.slider('What price range do you prefer?',10, 500, value=(50,250),step=10, key='price')
      room_type = st.multiselect('What room type do you want?',('Single room','double room','triple room','quad room','queen room','king room'))
      if st.button('Find me the rooms!!!',key='fireaway'):
-          Image = Image.open('./sample.jpg')
-          st.image(image, use_column_width=True)
+          col1,col2 = st.beta_columns(2)
+          with col1:
+              Image = Image.open('./sample.jpg')
+               st.image(image, use_column_width=True)
+          with col2:
+               df = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [1.35, 103.82],columns=['lat', 'lon'])
+               st.map(df)
 
 else:
      text = st.text_input('Your id')
